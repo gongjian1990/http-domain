d0c98729b6d16e2cdda73531baa6d178cff99caa
gongjian1990 <602192564@qq.com>
2019-03-30 15:39:04 +0800

    实体类添加

---
+++ src/main/java/com/didong/entity/TbVedioReport.java
@@ -0,0 +1,56 @@
+package com.didong.entity;
+
+import lombok.Data;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * <p>
+ * 用户关注表
+ * </p>
+ *
+ * @author jobob
+ * @since 2019-03-30
+ */
+@Data
+public class TbVedioReport implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键ID
+     */
+    private Long rId;
+
+    /**
+     * 视频ID
+     */
+    private Long vedioId;
+
+    /**
+     * 用户ID
+     */
+    private Long userId;
+
+    /**
+     * 视频ID
+     */
+    private Long reportUserId;
+
+    /**
+     * 举报原因
+     */
+    private String reportReason;
+
+    /**
+     * 点赞时间
+     */
+    private Date createTime;
+
+    /**
+     * 举报状态
+     */
+    private Boolean reportStatus;
+
+
+}
